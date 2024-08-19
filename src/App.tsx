4d731e2199252766ce5b9bbe476b0cfe42284252
Adel Armand <adel.armand@gmail.com>
2024-08-19 20:59:06 +0330

    Implement basic photo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+export interface Photos {
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 function App() {
+  const [photos, setPhotos] = React.useState<Photos[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const resp = await axios.get<Photos[]>('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10');
+      setPhotos(resp.data);
+    };
+    fetchData();
+  }, []);
+
+  if (photos.length === 0) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Create React App</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Image</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            photos.map(({ id, title, url }) => (
+              <tr key={id}>
+                <td>{title}</td>
+                <td><img width={300} height={300} alt={title} src={url} /></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
 
